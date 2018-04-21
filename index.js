da33d3465d130199039dcbce824e8e5edb9fb2d1
unknown <cy94295@gmail.com>
2018-04-21 23:15:02 +0800

    make it better

--- index.js
+++ index.js
@@ -211,23 +211,6 @@ bot.on('message', function (event) {
 						}
 					}
 					]);
-					event.reply({
-						type: 'template',
-						altText: 'this is a confirm template',
-						template: {
-						type: 'confirm',
-						text: 'Are you sure?',
-						actions: [{
-						type: 'message',
-						label: 'Yes',
-						text: 'yes'
-						}, {
-						type: 'message',
-						label: 'No',
-						text: 'no'
-						}]
-						}
-					});
 					break;
 			}
 	}
