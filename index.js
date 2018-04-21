e848ff57edb66618522b8deb16ec5d418f5e31cb
unknown <cy94295@gmail.com>
2018-04-21 23:28:31 +0800

    make it better

--- index.js
+++ index.js
@@ -194,20 +194,20 @@ bot.on('message', function (event) {
 						altText: '說明',
 						template: {
 						type: 'buttons',
-						title: '選單',
+						text: '選單',
 						actions: [{
-						  type: 'postback',
-						  label: 'Buy',
-						  data: 'action=buy&itemid=123'
-						}, {
-						  type: 'postback',
-						  label: 'Add to cart',
-						  data: 'action=add&itemid=123'
-						}, {
-						  type: 'uri',
-						  label: 'View detail',
-						  uri: 'http://example.com/page/123'
-						}]
+								type: 'message',
+								label: '選單',
+								text: '選單'
+							}, {
+								type: 'message',
+								label: '關於我們',
+								text: '關於我們'
+							}, {
+								type: 'message',
+								label: 't1',
+								text: 't1'
+							}]
 						}
 					},
 					/**
