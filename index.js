e381388af078cc55ed6ca0956ab89102579463d8
unknown <cy94295@gmail.com>
2018-05-04 21:19:23 +0800

    make it better

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ function Getjson(){
                     replymakeup.push(json[0].Summary[k].excerpt);
                     console.log(json[0].Summary[k].excerpt);
         }
+        return replymakeup;
     })
     .catch(function (err) {
         console.log('出錯了~找不到指定資源…');
