6d5e8a6e6fb35b72dd6f52e47cb02558b64a06be
unknown <cy94295@gmail.com>
2018-05-05 02:55:24 +0800

    make it better

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ function Gettime(){
 
 function Getjson(){
     Filename = Gettime();
+    replytheme=['','','','','','','','','','','','','','',''];
     console.log("http://projectkarubi.hopto.org/"+'talk/Food'+"/"+Filename+".json");
     var theme = ['talk/Food','talk/Makeup','talk/buyonline','talk/Talk','talk/3c','talk/acg','talk/boy','talk/finance','talk/game','talk/girl','talk/movie','talk/sport','talk/travel','talk/tvepisode','talk/vehicle'];
 
