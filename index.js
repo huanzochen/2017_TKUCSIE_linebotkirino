d7ce8cd7e1090aebd96365e8f2905cf3e8543697
unknown <cy94295@gmail.com>
2018-05-04 20:58:57 +0800

    make it better

--- index.js
+++ index.js
@@ -242,6 +242,7 @@ bot.on('message', function (event) {
 					break;
                 case '美妝':
                     //event.reply(json[0].Summary[0].excerpt);
+                    Getjson();
                     event.reply(replymakeup);
                     console.log(replymakeup);
                     break;
