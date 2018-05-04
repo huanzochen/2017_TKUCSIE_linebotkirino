a0ced0de03914715186556043c75107b6a50d625
unknown <cy94295@gmail.com>
2018-05-05 02:33:24 +0800

    make it better

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var j = schedule.scheduleJob('30 * * * * *', function(){
     var rd = Math.floor(Math.random()*15);
     bot.push(users, {
         type: 'text',
-        text: (reply+replytext[rd])
+        text: ('reply'+replytext[rd])
     });
     Getjson();
     console.log('rd:'+rd);
