429fa83945c2a2847722e6d4590fe18f1aadef1a
unknown <cy94295@gmail.com>
2018-05-22 02:50:18 +0800

    make it better

--- index.js
+++ index.js
@@ -767,7 +767,7 @@ bot.on('message', function (event) {
 });
 
 function Workjieba(event,ans){
-    event.reply('xx');
+    event.reply('x');
     
     food = nodejieba.cut(Fjson[0][0].topic);
     console.log('origin'+Fjson[0][0].topic);
