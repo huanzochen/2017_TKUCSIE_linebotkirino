60a0c746cf10f43c2914d8b507b268e754cb3179
unknown <cy94295@gmail.com>
2018-05-22 01:46:25 +0800

    make it better

--- index.js
+++ index.js
@@ -719,7 +719,7 @@ bot.on('message', function (event) {
 					});
 				break;
 				default:
-                    ans = nodejieba.cut(event.message.text);
+                    ans = nodejieba.cutHMM(event.message.text);
                     event.reply(ans);
                     
                     /*
