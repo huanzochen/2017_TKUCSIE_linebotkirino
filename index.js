cc60d770cdf723b923ed02a81239348efc44ecc3
unknown <cy94295@gmail.com>
2018-05-22 15:36:45 +0800

    make it better

--- index.js
+++ index.js
@@ -767,7 +767,7 @@ bot.on('message', function (event) {
 });
 
 function Workjieba(event,ans){
-    event.reply('aaa');
+    event.reply('aaaa');
     
     food = nodejieba.cut(Fjson[0][0].topic);
     console.log('origin'+Fjson[0][0].topic);
