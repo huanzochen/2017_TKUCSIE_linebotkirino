8b598175bf099c1cec76ee37a826f7e0154480f3
unknown <cy94295@gmail.com>
2018-05-23 11:20:36 +0800

    make it better

--- index.js
+++ index.js
@@ -907,7 +907,7 @@ function Workjieba(event,ans){
     
     
     
-    console.log('topic.length'+Fjson[0][0].topic.length+'ssssssssss'+Kfood);
+    console.log('topic.length'+Fjson[0][0].topic.length+'ssssssssssss'+Kfood);
 }
 
 app.listen(process.env.PORT || 80, function () {
